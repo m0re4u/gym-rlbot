1d684b1eb1168a793155486ce06bd06212f57280
Michiel van der Meer <michiel@dutchnaoteam.nl>
2019-03-09 00:04:45 +0100

    Add full state info

--- gym_rlbot/envs/rlbot_env.py
+++ gym_rlbot/envs/rlbot_env.py
@@ -21,16 +21,40 @@ class RLBotEnv(gym.Env):
             spaces.Discrete(2),
             spaces.Discrete(2)
             ))
-        self.observation_space = spaces.Box(low=np.array([-1.,-1]), high=np.array([1,1]), dtype=np.float)
         self.manager = runner.main(gym=True)
         self.game_thread = threading.Thread(target=self.manager.infinite_loop)
         self.game_thread.start()
+
+        self_location = spaces.Box(low=np.array([-4096., -5120., 0.]),       high=np.array([4096.,5120.,2044.]),      dtype=np.float)
+        self_rotation = spaces.Box(low=np.array([-np.pi, -np.pi, -np.pi]), high=np.array([np.pi, np.pi, np.pi]), dtype=np.float)
+        self_velocity = spaces.Box(low=np.array([0., 0., 0.]),               high=np.array([2300.,2300.,2300.]),    dtype=np.float)
+        self_angular_velocity = spaces.Box(low=np.array([-1., -1., -1.]), high=np.array([1., 1., 1.]), dtype=np.float)
+        has_wheel_contact = spaces.Discrete(2)
+        jumped = spaces.Discrete(2)
+        boost = spaces.Box(low=np.array([0]), high=np.array([100]), dtype=np.float)
+        ball_location = spaces.Box(low=np.array([-4096., -5120., 0.]),       high=np.array([4096.,5120.,2044.]),      dtype=np.float)
+        ball_rotation = spaces.Box(low=np.array([-np.pi, -np.pi, -np.pi]), high=np.array([np.pi, np.pi, np.pi]), dtype=np.float)
+        ball_velocity = spaces.Box(low=np.array([0., 0., 0.]),               high=np.array([2300.,2300.,2300.]),    dtype=np.float)
+        ball_angular_velocity = spaces.Box(low=np.array([-1., -1., -1.]), high=np.array([1., 1., 1.]), dtype=np.float)
+
+        self.observation_space = spaces.Tuple((
+            self_location, 
+            self_rotation, 
+            self_velocity, 
+            # self_angular_velocity, 
+            has_wheel_contact,
+            jumped,
+            boost,
+            ball_location,
+            ball_rotation,
+            ball_velocity,
+            # ball_angular_velocity,
+        ))
         
 
     def step(self, action):
         self.act(action)
         obs = self._get_obs()
-        print(obs)
 
         reward = 0
         scored = False
@@ -38,26 +62,44 @@ class RLBotEnv(gym.Env):
         return obs, reward, scored, {}
 
     def reset(self):
-        return 
+        obs = self._get_obs()
+        return obs
   
     def render(self, mode='human', close=False):
         pass
 
     def _get_obs(self):
         packet = self.manager.agent_state_queue.get()
-        obs = [ packet.game_cars[0].physics.location.x,  packet.game_cars[0].physics.location.y]
+        self_location = [packet.game_cars[0].physics.location.x,  packet.game_cars[0].physics.location.y, packet.game_cars[0].physics.location.z]
+        self_rotation = [packet.game_cars[0].physics.rotation.pitch, packet.game_cars[0].physics.rotation.yaw, packet.game_cars[0].physics.rotation.roll]
+        self_velocity = [packet.game_cars[0].physics.velocity.x, packet.game_cars[0].physics.velocity.y, packet.game_cars[0].physics.velocity.z]
+        ball_location = [packet.game_ball.physics.location.x,  packet.game_ball.physics.location.y, packet.game_ball.physics.location.z]
+        ball_rotation = [packet.game_ball.physics.rotation.pitch, packet.game_ball.physics.rotation.yaw, packet.game_ball.physics.rotation.roll]
+        ball_velocity = [packet.game_ball.physics.velocity.x, packet.game_ball.physics.velocity.y, packet.game_ball.physics.velocity.z]
+
+        obs = [
+            *self_location,
+            *self_rotation,
+            *self_velocity,
+            packet.game_cars[0].has_wheel_contact,
+            packet.game_cars[0].jumped,
+            packet.game_cars[0].boost,
+            *ball_location,
+            *ball_rotation,
+            *ball_velocity
+        ]
         return obs
 
     def act(self, action):
         controller_state = SimpleControllerState()
-        controller_state.throttle = action[0][0]
-        controller_state.steer = action[0][1]
-        controller_state.pitch = action[0][2]
-        controller_state.yaw = action[0][3]
-        controller_state.roll = action[0][4]
-        controller_state.jump =action[1]
-        controller_state.boost = action[2]
-        controller_state.handbrake = action[3]
+        controller_state.throttle = action[0]
+        controller_state.steer = action[1]
+        controller_state.pitch = action[2]
+        controller_state.yaw = action[3]
+        controller_state.roll = action[4]
+        controller_state.jump = True if action[5] > 0.5 else False
+        controller_state.boost = True if action[6] > 0.5 else False
+        controller_state.handbrake = True if action[7] > 0.5 else False
         self.manager.agent_action_queue.put(controller_state)
 
 
@@ -143,34 +185,3 @@ def draw_debug(renderer, car, ball, action_display):
 
 
 
-# # 'physics': {
-# #     'location': {'x': 0.0, 'y': 0.0, 'z': 0.0},
-# #     'rotation': {'pitch': 0.0, 'yaw': 0.0, 'roll': 0.0},
-# #     'velocity': {'x': 0.0, 'y': 0.0, 'z': 0.0},
-# #     'angular_velocity': {'x': 0.0, 'y': 0.0, 'z': 0.0}
-# # },
-# self_location = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# self_rotation = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# self_velocity = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# self_angular_velocity = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# has_wheel_contact = spaces.Discrete(2)
-# jumped = spaces.Discrete(2)
-# boost = spaces.Box(low=np.array([0]), high=np.array([100]), dtype=np.float)
-# ball_location = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# ball_rotation = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# ball_velocity = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-# ball_angular_velocity = spaces.Box(low=np.array([-1., -1., -1]), high=np.array([1., 1., 1]), dtype=np.float)
-
-# self.observation_space = spaces.Tuple((
-#   self_location, 
-#   self_rotation, 
-#   self_velocity, 
-#   self_angular_velocity, 
-#   has_wheel_contact,
-#   jumped,
-#   boost,
-#   ball_location,
-#   ball_rotation,
-#   ball_velocity,
-#   ball_angular_velocity,
-#   ))

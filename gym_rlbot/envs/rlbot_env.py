fbee175131bfdf2217481d3095de72756e35fd0d
Michiel van der Meer <michiel@dutchnaoteam.nl>
2019-03-09 17:19:53 +0100

    Limited environment -> only throttle action

--- gym_rlbot/envs/rlbot_env.py
+++ gym_rlbot/envs/rlbot_env.py
@@ -16,10 +16,11 @@ class RLBotEnv(gym.Env):
     def __init__(self):
         super(RLBotEnv, self).__init__()
         self.action_space = spaces.Tuple((
-            spaces.Box(low=np.array([-1., -1., -1., -1., -1.]), high=np.array([1., 1., 1., 1., 1.]), dtype=np.float),
-            spaces.Discrete(2),
-            spaces.Discrete(2),
-            spaces.Discrete(2)
+            spaces.Box(low=np.array([-1.]), high=np.array([1.]), dtype=np.float),
+            # spaces.Box(low=np.array([-1., -1., -1., -1., -1.]), high=np.array([1., 1., 1., 1., 1.]), dtype=np.float),
+            # spaces.Discrete(2),
+            # spaces.Discrete(2),
+            # spaces.Discrete(2)
             ))
         self.manager = runner.main(gym=True)
         self.game_thread = threading.Thread(target=self.manager.infinite_loop)
@@ -29,27 +30,27 @@ class RLBotEnv(gym.Env):
 
         self_location = spaces.Box(low=np.array([-4096., -5120., 0.]),       high=np.array([4096.,5120.,2044.]),      dtype=np.float)
         self_rotation = spaces.Box(low=np.array([-np.pi, -np.pi, -np.pi]), high=np.array([np.pi, np.pi, np.pi]), dtype=np.float)
-        self_velocity = spaces.Box(low=np.array([0., 0., 0.]),               high=np.array([2300.,2300.,2300.]),    dtype=np.float)
-        self_angular_velocity = spaces.Box(low=np.array([-1., -1., -1.]), high=np.array([1., 1., 1.]), dtype=np.float)
-        has_wheel_contact = spaces.Discrete(2)
-        jumped = spaces.Discrete(2)
-        boost = spaces.Box(low=np.array([0]), high=np.array([100]), dtype=np.float)
+        # self_velocity = spaces.Box(low=np.array([0., 0., 0.]),               high=np.array([2300.,2300.,2300.]),    dtype=np.float)
+        # self_angular_velocity = spaces.Box(low=np.array([-1., -1., -1.]), high=np.array([1., 1., 1.]), dtype=np.float)
+        # has_wheel_contact = spaces.Discrete(2)
+        # jumped = spaces.Discrete(2)
+        # boost = spaces.Box(low=np.array([0]), high=np.array([100]), dtype=np.float)
         ball_location = spaces.Box(low=np.array([-4096., -5120., 0.]),       high=np.array([4096.,5120.,2044.]),      dtype=np.float)
-        ball_rotation = spaces.Box(low=np.array([-np.pi, -np.pi, -np.pi]), high=np.array([np.pi, np.pi, np.pi]), dtype=np.float)
-        ball_velocity = spaces.Box(low=np.array([0., 0., 0.]),               high=np.array([2300.,2300.,2300.]),    dtype=np.float)
-        ball_angular_velocity = spaces.Box(low=np.array([-1., -1., -1.]), high=np.array([1., 1., 1.]), dtype=np.float)
+        # ball_rotation = spaces.Box(low=np.array([-np.pi, -np.pi, -np.pi]), high=np.array([np.pi, np.pi, np.pi]), dtype=np.float)
+        # ball_velocity = spaces.Box(low=np.array([0., 0., 0.]),               high=np.array([2300.,2300.,2300.]),    dtype=np.float)
+        # ball_angular_velocity = spaces.Box(low=np.array([-1., -1., -1.]), high=np.array([1., 1., 1.]), dtype=np.float)
 
         self.observation_space = spaces.Tuple((
             self_location,
             self_rotation,
-            self_velocity,
+            # self_velocity,
             # self_angular_velocity,
-            has_wheel_contact,
-            jumped,
-            boost,
+            # has_wheel_contact,
+            # jumped,
+            # boost,
             ball_location,
-            ball_rotation,
-            ball_velocity,
+            # ball_rotation,
+            # ball_velocity,
             # ball_angular_velocity,
         ))
 
@@ -60,7 +61,6 @@ class RLBotEnv(gym.Env):
 
         if scored:
             reward = 0
-            self.score += 1
         else:
             reward = -1
 
@@ -87,57 +87,37 @@ class RLBotEnv(gym.Env):
         obs = [
             *self_location,
             *self_rotation,
-            *self_velocity,
-            packet.game_cars[0].has_wheel_contact,
-            packet.game_cars[0].jumped,
-            packet.game_cars[0].boost,
+            # *self_velocity,
+            # packet.game_cars[0].has_wheel_contact,
+            # packet.game_cars[0].jumped,
+            # packet.game_cars[0].boost,
             *ball_location,
-            *ball_rotation,
-            *ball_velocity
+            # *ball_rotation,
+            # *ball_velocity
         ]
 
         scored = packet.teams[0].score > self.score
+        self.score = packet.teams[0].score
         return obs, scored
 
     def act(self, action):
         controller_state = SimpleControllerState()
         controller_state.throttle = action[0]
-        controller_state.steer = action[1]
-        controller_state.pitch = action[2]
-        controller_state.yaw = action[3]
-        controller_state.roll = action[4]
-        controller_state.jump = True if action[5] > 0.5 else False
-        controller_state.boost = True if action[6] > 0.5 else False
-        controller_state.handbrake = True if action[7] > 0.5 else False
+        # controller_state.steer = action[1]
+        # controller_state.pitch = action[2]
+        # controller_state.yaw = action[3]
+        # controller_state.roll = action[4]
+        # controller_state.jump = True if action[5] > 0.5 else False
+        # controller_state.boost = True if action[6] > 0.5 else False
+        # controller_state.handbrake = True if action[7] > 0.5 else False
+        controller_state.steer = 0
+        controller_state.pitch = 0
+        controller_state.yaw = 0
+        controller_state.roll = 0
+        controller_state.jump = 0
+        controller_state.boost = 0
+        controller_state.handbrake = 0
         self.manager.agent_action_queue.put(controller_state)
-
-
-class Vector2:
-    def __init__(self, x=0, y=0):
-        self.x = float(x)
-        self.y = float(y)
-
-    def __add__(self, val):
-        return Vector2(self.x + val.x, self.y + val.y)
-
-    def __sub__(self, val):
-        return Vector2(self.x - val.x, self.y - val.y)
-
-    def correction_to(self, ideal):
-        # The in-game axes are left handed, so use -x
-        current_in_radians = math.atan2(self.y, -self.x)
-        ideal_in_radians = math.atan2(ideal.y, -ideal.x)
-
-        correction = ideal_in_radians - current_in_radians
-
-        # Make sure we go the 'short way'
-        if abs(correction) > math.pi:
-            if correction < 0:
-                correction += 2 * math.pi
-            else:
-                correction -= 2 * math.pi
-
-        return correction
 
 
 class GymAgent(BaseAgent):
@@ -147,28 +127,12 @@ class GymAgent(BaseAgent):
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         # All stuff here does not matter anymore, controller state is handled through the gym environment
-        ball_location = Vector2(packet.game_ball.physics.location.x, packet.game_ball.physics.location.y)
-
-        self.obs = [packet.game_ball.physics.location.x, packet.game_ball.physics.location.y]
-
 
         my_car = packet.game_cars[self.index]
-        self.controller_state.throttle = 1.0
-        self.controller_state.steer = 0.0
-
         draw_debug(self.renderer, my_car, packet.game_ball, "test")
 
-        return self.controller_state
+        return None
 
-
-def get_car_facing_vector(car):
-    pitch = float(car.physics.rotation.pitch)
-    yaw = float(car.physics.rotation.yaw)
-
-    facing_x = math.cos(pitch) * math.cos(yaw)
-    facing_y = math.cos(pitch) * math.sin(yaw)
-
-    return Vector2(facing_x, facing_y)
 
 def draw_debug(renderer, car, ball, action_display):
     renderer.begin_rendering()
